55ea2eac7cb38f3d1946bd0945ea1b669588152e
Athanasios <athana.stathis@gmail.com>
2022-05-18 02:06:57 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, Response, status, HTTPException
+from typing import Optional
+
+from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
+
+app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [{"title": "Title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "Title of post 2", "content": "content of post 2", "id": 2}]
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Greece!!!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+
+@app.get("/posts/{id}")  # id: str
+def get_posts(id: int):
+    post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with {id} was not found")
+    return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
+    my_posts.pop(index)
+    # return {'message': "Post was successfully deleted"}
+    # gives RuntimeError: Response content longer than Content-Length
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {'data': post_dict}
+
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
+
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
